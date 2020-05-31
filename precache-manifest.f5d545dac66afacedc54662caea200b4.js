e62df2a4e2527338cce57cb9d6c5d73e94642375
Nishant Choudhary <n1sh4ntch0udh4ry@gmail.com>
2020-06-01 02:35:54 +0530

    Updates

---
+++ precache-manifest.f5d545dac66afacedc54662caea200b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fecb76a469c79224ed7d2c43806104ee",
+    "url": "/simple-pokedex/index.html"
+  },
+  {
+    "revision": "a97fbf07851c901bb6b4",
+    "url": "/simple-pokedex/static/css/main.b18f1e29.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/simple-pokedex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-pokedex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97fbf07851c901bb6b4",
+    "url": "/simple-pokedex/static/js/main.fd337320.chunk.js"
+  },
+  {
+    "revision": "9e45407a4178a8ebba57",
+    "url": "/simple-pokedex/static/js/runtime-main.f28bc44e.js"
+  }
+]);
